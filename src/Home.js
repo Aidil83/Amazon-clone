4a7fe4b7bd5bfff806b45e0ed5b7e3c2713f994a
Aidil <aidilrozi83@gmail.com>
2020-08-02 13:07:23 -0500

    added images for each card product

--- src/Home.js
+++ src/Home.js
@@ -14,50 +14,50 @@ function Home() {
 			<div className='home__row'>
 				<Product
 					id='123455'
-					title='the lean startup: how constant innovation creates radically successful businesses paperback'
-					price={11.96}
+					title='Staples Hyken Technical Mesh Task Chair (Black) - Adjustable Office Chair with Breathable Mesh Material, Provides Lumbar, arm and Head Support.'
+					price={219.95}
 					rating={5}
-					image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg'
+					image='https://m.media-amazon.com/images/I/8189i6pt7aL._AC_UL320_.jpg'
 				/>
 				<Product
-					id='123455'
-					title='The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback'
+					id='123456'
+					title='Over the Sink Dish Drying Rack, iSPECLE Large Premium 201 Stainless Steel Dish Rack with Utensil Holder Hooks for Kitchen Counter Non-slip'
 					price={11.96}
-					rating={5}
-					image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg'
+					rating={4}
+					image='https://images-na.ssl-images-amazon.com/images/I/611T39OilfL._AC_SL1000_.jpg'
 				/>
 			</div>
 			<div className='home__row'>
 				<Product
-					id='123455'
+					id='123457'
 					title='The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback'
 					price={11.96}
-					rating={5}
+					rating={3}
 					image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg'
 				/>
 				<Product
-					id='123455'
-					title='The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback'
-					price={11.96}
-					rating={5}
-					image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg'
+					id='123458'
+					title='Laptop Backpack, Slim Durable Laptops Backpack with USB Charging Port'
+					price={31.99}
+					rating={4}
+					image='https://images-na.ssl-images-amazon.com/images/I/81idlqFqcUL._AC_SL1500_.jpg'
 				/>
 				<Product
-					id='123455'
-					title='The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback'
-					price={11.96}
-					rating={5}
-					image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg'
+					id='123459'
+					title='Gaming Headset for Xbox One & PS4, AIMASON Noise Cancelling Over Ear Headphone'
+					price={46.34}
+					rating={2}
+					image='https://images-na.ssl-images-amazon.com/images/I/71AmD4ZUnGL._AC_SL1500_.jpg'
 				/>
 			</div>
 
 			<div className='home__row'>
 				<Product
-					id='123455'
-					title='The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback'
-					price={11.96}
+					id='123450'
+					title='Samsung CJ890 Series 49 inch 3840x1080 Super Ultra-Wide Desktop Monitor for Business, 144 Hz, USB-C, HDMI, DisplayPort, 3-Year Warranty (C49J890DKN)'
+					price={1099.79}
 					rating={5}
-					image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg'
+					image='https://images-na.ssl-images-amazon.com/images/I/81B0xCF%2BPWL._AC_SL1500_.jpg'
 				/>
 			</div>
 		</div>
